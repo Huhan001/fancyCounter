7a0b3a4817a6ec9c46660304bf4a763b8b2d42e7
Humphrey Hanson <114668148+Huhan001@users.noreply.github.com>
2024-08-13 23:03:10 +0200

    a cleaner way to set back to zero without needed to reach the previous value.

--- src/ResetButton.jsx
+++ src/ResetButton.jsx
@@ -3,11 +3,11 @@ import style from './Project.module.css';
 
 const ResetButton = ({setNumber}) => {
     // i find this to be most convenient way as to create a function and pass it as an event handler button onClick event.
-    const minus = () => {
-        setNumber(prev => prev = 0);
+    const setZero = () => {
+        setNumber(0);
     }
     return (
-        <button onClick={minus} className= {style.reset_btn}>
+        <button onClick={setZero} className= {style.reset_btn}>
             <ResetIcon  className= {style.reset_btn_icon}/>
         </button>
     );
