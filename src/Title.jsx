0fdf82ce4908c91a73754d2259459a1dd5c06b4d
Humphrey Hanson <114668148+Huhan001@users.noreply.github.com>
2024-08-14 20:04:53 +0200

    feat/adjust/Tittle: adjusted text

--- src/Title.jsx
+++ src/Title.jsx
@@ -3,7 +3,7 @@ import style from "./Project.module.css";
 export const Title = ({ locked }) => {
   return (
     <h1 className={style.title}>
-      {locked ? "Reached your Limit > 5" : "Fancy Counter"}
+      {locked ? "Limit! Buy Pro > 5" : "Fancy Counter"}
     </h1>
   );
 };
